96f7e0c54fc657af253d995cc34e0421a3bfb29b
brijeshpthankachan <brijeshpthankachan@gmail.com>
2023-10-27 12:03:19 +0530

    Added Register View Model

---
+++ IdentityMVC/Models/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityMVC.Models;
+
+public class RegisterViewModel
+{
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email")]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long", MinimumLength = 4)]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string Password { get; set; } = null!;
+
+    [Required]
+    [Display(Name = "Confirm Password")]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Passwords must match")]
+    public string ConfirmPassword { get; set; } = null!;
+
+    [Required] public string Name { get; set; } = null!;
+}
