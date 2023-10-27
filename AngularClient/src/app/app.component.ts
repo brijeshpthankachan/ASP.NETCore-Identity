d9625010dc051946596a49ebe346c21d532d3ea9
brijeshpthankachan <brijeshpthankachan@gmail.com>
2023-10-27 16:28:03 +0530

    removed unwanted packages

--- AngularClient/src/app/app.component.ts
+++ AngularClient/src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'AngularClient';
