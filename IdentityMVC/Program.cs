35f8c7359d657a7168e3301f80f0159a3ccac7b6
brijeshpthankachan <brijeshpthankachan@gmail.com>
2023-10-27 11:44:09 +0530

    configured Identity and identity options

--- IdentityMVC/Program.cs
+++ IdentityMVC/Program.cs
@@ -1,4 +1,5 @@
 using IdentityMVC.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,7 +19,27 @@ app.Run();
 
 void AddServices(IServiceCollection services)
 {
+    // This method configures the MVC services for the commonly used features with controllers and views.
+    // Note that this method does not register services used for pages.
     services.AddControllersWithViews();
+
+    // Registers the given context as a service.
     services.AddDbContext<ApplicationDbContext>(dbConextOptions =>
         dbConextOptions.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL")));
+
+    // Adds The Default Identity configurations for the specified user and role.
+    services.AddIdentity<IdentityUser, IdentityRole>()
+        .AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+    // Configuring IdentityOptions for Authentication and Password Policy Settings
+    // Defines and register an action that configures the IdentityOptions.
+    // https://stackoverflow.com/questions/53424593/services-configure-or-services-addsingleton-get
+    services.Configure<IdentityOptions>(opt =>
+    {
+        opt.Password.RequiredLength = 5;
+        opt.Password.RequireLowercase = true;
+        opt.Lockout.MaxFailedAccessAttempts = 2;
+        opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(30);
+        opt.SignIn.RequireConfirmedEmail = false;
+    });
 }
