6d025a229ee35ab396e6142fe89deed21b3824a5
brijeshpthankachan <brijeshpthankachan@gmail.com>
2023-10-27 12:31:11 +0530

    configured app to use authentication

--- IdentityMVC/Program.cs
+++ IdentityMVC/Program.cs
@@ -11,6 +11,7 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 app.Run();
