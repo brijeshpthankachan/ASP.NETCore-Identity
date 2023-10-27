bf18cc28b0febb2466f16f2d2e1e5e14122c9b85
brijeshpthankachan <brijeshpthankachan@gmail.com>
2023-10-27 16:11:45 +0530

    Added Identity User

--- IdentityWebApi/Data/ApplicationDbContext.cs
+++ IdentityWebApi/Data/ApplicationDbContext.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using IdentityWebApi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace IdentityWebApi.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-    {
-    }
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 }
---
+++ IdentityWebApi/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityWebApi.Models;
+
+public class ApplicationUser : IdentityUser
+{
+    [Required] public string Name { get; set; } = null!;
+}
