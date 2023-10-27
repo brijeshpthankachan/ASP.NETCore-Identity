d57cfd45d5d9c81179c2891e5bdc00b265715376
brijeshpthankachan <brijeshpthankachan@gmail.com>
2023-10-27 12:35:39 +0530

    Added Forgot password view model

---
+++ IdentityMVC/Models/ForgotPasswordViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityMVC.Models;
+
+public class ForgotPasswordViewModel
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = null!;
+}
