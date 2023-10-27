14deb4fb241aace1a3b19cfa0a4e08515c4b3456
brijeshpthankachan <brijeshpthankachan@gmail.com>
2023-10-27 12:23:37 +0530

    Added Login View

--- IdentityMVC/Controllers/AccountController.cs
+++ IdentityMVC/Controllers/AccountController.cs
@@ -30,4 +30,12 @@ public class AccountController : Controller
         if (!result.Succeeded) return View(model);
         return LocalRedirect("/");
     }
+
+    [HttpGet]
+    public IActionResult Login(string? returnUrl = null)
+    {
+        ViewData["ReturnUrl"] = returnUrl;
+        return View();
+    }
+
 }
---
+++ IdentityMVC/Models/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityMVC.Models;
+
+public class LoginViewModel
+{
+    [Required]
+    [EmailAddress]
+    [Display(Name = "Email")]
+    public string Email { get; set; } = null!;
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
+
+    [Required]
+    [Display(Name = "Remember me?")]
+    public bool RememberMe { get; set; }
+}
