703c412c8def6916e3464e036c8f996952b8cd7a
brijeshpthankachan <brijeshpthankachan@gmail.com>
2023-10-27 12:01:44 +0530

    Added Account Controller and register view

---
+++ IdentityMVC/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityMVC.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    [HttpGet]
+    public IActionResult Register() => View();
+}
