bbfff38d0ecc76e1e121d99f37d8ae8d38c6782f
brijeshpthankachan <brijeshpthankachan@gmail.com>
2023-10-27 16:08:59 +0530

    Added Db Context and identity

--- IdentityWebApi/Program.cs
+++ IdentityWebApi/Program.cs
@@ -1,15 +1,12 @@
+using IdentityWebApi.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+AddServices(builder.Services);
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +14,28 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
+
+void AddServices(IServiceCollection services)
+{
+    services.AddControllers();
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen();
+    services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQL")));
+    services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+    services.Configure<IdentityOptions>(opt =>
+    {
+        opt.Password.RequiredLength = 5;
+        opt.Password.RequireLowercase = true;
+        opt.Lockout.MaxFailedAccessAttempts = 2;
+        opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromSeconds(30);
+    });
+    services.AddAuthentication().AddGoogle(options =>
+    {
+        options.ClientId = "912586231632-7747sd0jqkn9pk9f53r9le613dphacsd.apps.googleusercontent.com";
+        options.ClientSecret = "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";
+    });
+}
